f11e0d25c6dfa24c9a5a2210b4a62468f0d0eaab
Takanori Ishikawa <ishikawa.takanori@abejainc.com>
2022-11-05 08:13:25 +0900

    build

---
+++ ffi/build.rs
@@ -0,0 +1,18 @@
+use camino::Utf8Path;
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    uniffi_build::generate_scaffolding("./src/lib.udl").unwrap();
+    uniffi_bindgen::generate_bindings(
+        Utf8Path::new("./src/lib.udl"),
+        None,
+        vec!["swift"],
+        Utf8Path::from_path(Path::new(&out_dir)),
+        None,
+        false,
+    )
+    .unwrap();
+}
--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,3 +1,11 @@
+use uniffi_macros;
+
+uniffi_macros::include_scaffolding!("lib");
+
+fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
 #[no_mangle]
 pub extern "C" fn hello_world() {
     println!("Hello World!");
