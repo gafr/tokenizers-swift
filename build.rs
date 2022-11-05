3e663ec2c6ab35967f9953fbd9fc91d8bc7d9bcd
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-11-05 20:19:06 +0900

    add uniffi config

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     uniffi_build::generate_scaffolding("./src/lib.udl").unwrap();
     uniffi_bindgen::generate_bindings(
         Utf8Path::new("./src/lib.udl"),
-        None,
+        Some(Utf8Path::new("./uniffi.toml")),
         vec!["swift"],
         Utf8Path::from_path(Path::new(&out_dir)),
         None,
